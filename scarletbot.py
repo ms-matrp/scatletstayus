ea871f0de3a1877cc8c8cebf4656c0159ac5022e
Maxim Shestak <80178152+ms-matrp@users.noreply.github.com>
2026-05-24 02:38:35 +0300

    Update scarletbot.py

--- scarletbot.py
+++ scarletbot.py
@@ -1,40 +1,34 @@
 import telebot
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 TOKEN = "8314373495:AAGhoKY1m6AcMC23BmvYLkKv6XHKMnFjHks"
 
 bot = telebot.TeleBot(TOKEN)
 
 
-def get_status():
-    url = "https://usescarlet.com"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
-    response = print(response.text)
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    status = soup.find("h1", class_="status svelte-1w0cweh")
-
-    if status:
-        return status.text.strip()
-
-    return "Status not found"
-
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    status = get_status()
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install())
+    )
+
+    driver.get("https://usescarlet.com")
+
+    status = driver.find_element(
+        By.CLASS_NAME,
+        "status svelte-1w0cweh"
+    ).text
 
     bot.send_message(
         message.chat.id,
-        f"Привет! Это бот в котором ты можешь посмотреть статус Scarlet!\n\Статус взят с сайта - usescarlet.com\n\Актуальный статус: {status}"
+        f"🔥 Scarlet Status: {status}"
     )
 
+    driver.quit()
 
-print("Bot started")
+
 bot.infinity_polling()
